acabc7f18bcc0ad79d98ed22920ecc6157dee936
Alexander <Sasha2dx@gmail.com>
2020-09-11 17:01:06 +0300

    Add files via upload

--- play.js
+++ play.js
@@ -1,3 +1,23 @@
-let getParams = "";
+let ps;
+let game;
 
-let iframe = document.getElementById("game");
+let getItems = location.search.substr(1).split("&");
+
+for (let index = 0; index < getItems.length; index++) {
+    let tmp = getItems[index].split("=");
+    if (tmp[0] === 'g') game = tmp[1];
+    else if (tmp[0] === 'ps') ps = tmp[1];
+}
+
+if (game){
+
+    let url = 'https://holy-games.herokuapp.com/play/'+game
+    if (ps) url +='?ps='+ps;
+    url += location.hash;
+
+    let iframe = document.getElementById("game");
+    iframe.src = url;
+
+} else {
+    window.location.href = "https://holy-games.space";
+}
